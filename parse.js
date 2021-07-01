a45498b90ba67c61992637280842b4457cef3b2e
Rio Ogino <rogino@users.noreply.github.com>
2021-07-01 14:34:09 +1200

    Update CSS, add footnotes

--- parse.js
+++ parse.js
@@ -1,5 +1,5 @@
-
 const hljs = require("highlight.js");
+const anchor = require("markdown-it-anchor");
 const md = require('markdown-it')({
   html:         true,        // Enable HTML tags in source
   linkify:      true,        // Autoconvert URL-like text to links
@@ -7,6 +7,7 @@ const md = require('markdown-it')({
   // Enable some language-neutral replacement + quotes beautification
   // For the full list of replacements, see https://github.com/markdown-it/markdown-it/blob/master/lib/rules_core/replacements.js
   typographer:  false,
+
   highlight: (str, lang) => {
     if (lang && hljs.getLanguage(lang)) {
       try {
@@ -19,9 +20,15 @@ const md = require('markdown-it')({
   }
 });
 
-md.use(require("markdown-it-anchor"));
-md.use(require("markdown-it-toc-done-right"));
 
+md.use(anchor, {
+  permalink: anchor.permalink.headerLink()
+});
+
+md.use(require("markdown-it-toc-done-right"), {
+  level: 2
+});
+md.use(require("markdown-it-footnote"));
 md.use(require("markdown-it-texmath"), {
   engine: require("katex"),
   delimiters: "dollars"
@@ -34,8 +41,31 @@ const fs = require("fs");
 
 let contents = fs.readFileSync("./COSC261/2. Regular Expressions.md", { encoding: "utf8"});
 
+contents = `# H1
 
-// Add [toc] if not already present
+## H2 first H2
+
+![test image](https://via.placeholder.com/1000x500)
+
+![test image](https://via.placeholder.com/800x2000)
+
+> Working software is the primary measure of progress
+>
+> *Agile Manifesto, 7th Principle*
+> 
+> > Another blockquote
+> > Recursion
+
+|   While designing   |      Prepare      |
+|---------------------|-------------------|
+|     Requirements    | Acceptance tests  |
+| System requirements |    System tests   |
+|    Global design    | Integration tests |
+|   Detailed design   |    Unit tests     |
+|        UI           |     ~~Scream~~    |
+
+
+\`\`\`js
 const lines = contents.split("\n");
 if (
     lines.length > 3 &&
@@ -45,6 +75,68 @@ if (
 ) {
   contents = lines[0] + "\n[toc]\n" + lines.slice(3).join("\n");
 }
+\`\`\`
+
+**bold** and then *italics* then ***both***
+
+\`inline code block\`
+
+### H3
+
+adsfasdf
+
+## H2-2
+
+### H3-2
+
+asdf
+
+#### H4-1
+
+adsf
+
+##### Line Breaks
+
+Apparently two spaces at end is a line break.  
+There should be a line break
+
+##### HRs
+
+Line 1
+
+***
+
+Three asterisks 
+
+##### Links
+
+[google](https://google.com "title")
+
+##### Footnotes
+
+- Using GDELT's GCAM[^y] columns, create a list of all dimensions encountered. This is necessary as the data needs to be put in a dense matrix/dataframe
+- As there are ~2000 of them, many of which are not relevant to the research question (e.g. \`FISHERIES\` from 'Lexicoder Topic Dictionaries' doesn't indicate the tone of the article), this needed to be pruned. One dictionary, WordNet Affect 1.1 is used. This alone has over 200 dimensions that measure sentiment, and so this was deemed sufficient
+
+[^y]: One interesting note is that of the 40 or so dictionaries found in the [GCAM MASTER CODEBOOK](http://data.gdeltproject.org/documentation/GCAM-MASTER-CODEBOOK.TXT), The 'Central Bank Financial Stability Report Sentiment' dictionary has an ID of \`40\` but each dimension has a value of \`c41.x\` - this made for an interesting debugging session.
+
+##### Reference-style link
+
+[This is a link][1]
+
+[1]: google.com
+`;
+
+
+// Add [toc] if not already present
+const lines = contents.split("\n");
+if (
+    lines.length > 3 &&
+    lines[0].match(/^# .+/) &&
+    lines[1].trim().length == 0 &&
+    !lines[2].match(/^\[toc\]$/)
+) {
+  contents = lines[0] + "\n[toc]\n" + lines.slice(2).join("\n");
+}
 
 
 fs.writeFileSync("./out.html", `
@@ -52,15 +144,15 @@ fs.writeFileSync("./out.html", `
 <html>
 <head>
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex/dist/katex.min.css">
   <link rel="stylesheet" href="./monokai-light.css">
   <link rel="stylesheet" href="./main.css">
   <title>test</title>
 </head>
 <body>
-  <div>
+  <article>
     ${md.render(contents)}
-  </div>
+  </article>
 </body>
 `);
