07ec546d81e98e84d24a10c89306700c18b07c8c
Rio Ogino <rogino@users.noreply.github.com>
2021-08-31 17:54:37 +1200

    Enable markdown-it typographer

--- compile_to_html/render-markdown.js
+++ compile_to_html/render-markdown.js
@@ -6,7 +6,7 @@ const md = require('markdown-it')({
 
   // Enable some language-neutral replacement + quotes beautification
   // For the full list of replacements, see https://github.com/markdown-it/markdown-it/blob/master/lib/rules_core/replacements.js
-  typographer:  false,
+  typographer:  true,
 
   highlight: (str, lang) => {
     if (lang && hljs.getLanguage(lang)) {
